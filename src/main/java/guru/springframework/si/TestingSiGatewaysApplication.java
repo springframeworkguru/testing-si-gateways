1501be1dfd902b79e4fddc97cb958d28812cb38c
John Thompson <john@springframework.guru>
2015-04-30 14:30:29 -0400

    adding base Spring Boot project

---
+++ src/main/java/guru/springframework/si/TestingSiGatewaysApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSiGatewaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSiGatewaysApplication.class, args);
+    }
+}
---
+++ src/test/java/guru/springframework/si/TestingSiGatewaysApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.si;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestingSiGatewaysApplication.class)
+public class TestingSiGatewaysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
